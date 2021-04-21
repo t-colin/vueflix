e1829aaae1a985dc308248b0fe413e76efe729f2
Thomas Colin <thomas.colin@le-campus-numerique.fr>
2021-04-21 13:54:19 +0200

    deploy

--- vueflix/deploy.sh
+++ vueflix/deploy.sh
@@ -20,6 +20,6 @@ git commit -m 'deploy'
 # git push -f git@github.com:<USERNAME>/<USERNAME>.github.io.git master
 
 # if you are deploying to https://<USERNAME>.github.io/<REPO>
- git push -f git@github.com:colin-thom/JS_vue-Vueflix-2021.git master:gh-pages
+ git push -f git@github.com:colin-thom/JS_vue-Vueflix-2021.git main:gh-pages
 
 cd -
